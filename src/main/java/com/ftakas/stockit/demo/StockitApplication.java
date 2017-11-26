e670ce7ba7a70f4ae94c75bd3ce60a01c8f20c18
Pafsanias Ftakas <pafsanias@ftakas.com>
2017-11-26 14:03:01 +0200

    Initial commit

---
+++ src/main/java/com/ftakas/stockit/demo/StockitApplication.java
@@ -0,0 +1,12 @@
+package com.ftakas.stockit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockitApplication.class, args);
+	}
+}
